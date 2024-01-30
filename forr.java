55173b44ec5e3794adca5a1459b74d3a746c0646
NguyenAnhHao1009 <anhhaoaoao@gmail.com>
2024-01-30 09:27:53 +0700

    commit

--- forr.java
+++ forr.java
@@ -1,7 +1,7 @@
 import util.Scanner.java;
 
 public static forr{
-    public static void main(String[]args){
+    public static void main(String []args){
         int i;
 		for(i=1; i<=10; i++){
 			System.out.println(i);
